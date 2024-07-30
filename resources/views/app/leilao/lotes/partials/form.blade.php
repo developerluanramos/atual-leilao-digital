c2d8cf74048e7e8a5695f179c97d2e686a2b73c8
= <=>
2024-07-30 19:05:52 -0300

    lotes e itens

--- resources/views/app/leilao/lotes/partials/form.blade.php
+++ resources/views/app/leilao/lotes/partials/form.blade.php
@@ -55,7 +55,7 @@
             Comissão Compra
         </label>
         <label class="inline-flex items-center me-5 cursor-pointer">
-            <input name="incide_comissao_compra" type="checkbox" value="{{$lote->incide_comissao_compra ?? old('incide_comissao_compra')}}" class="sr-only peer">
+            <input name="incide_comissao_compra" type="checkbox" value="true" class="sr-only peer">
             <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
             <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
         </label>
@@ -71,7 +71,7 @@
             Comissão Venda
         </label>
         <label class="inline-flex items-center me-5 cursor-pointer">
-            <input name="incide_comissao_venda" type="checkbox" value="{{$lote->incide_comissao_venda ?? old('incide_comissao_venda')}}" class="sr-only peer">
+            <input name="incide_comissao_venda" type="checkbox" value="true" class="sr-only peer">
             <div class="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
             <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
         </label>
