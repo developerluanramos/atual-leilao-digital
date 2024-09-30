2bd47d56c1b555e84792168f507ded639e2f0198
Thyerre <thyerre.sampaio@kolaresti.com.br>
2024-09-30 14:56:21 -0300

    CLD-10 - excluindo comentarios action

--- app/Actions/PlanoPagamento/PlanoPagamentoDeleteAction.php
+++ app/Actions/PlanoPagamento/PlanoPagamentoDeleteAction.php
@@ -2,7 +2,6 @@
 
 namespace App\Actions\PlanoPagamento;
 
-use App\Models\CondicaoPagamento;
 use App\Repositories\PlanoPagamento\PlanoPagamentoRepositoryInterface;
 
 class PlanoPagamentoDeleteAction
@@ -14,8 +13,6 @@ class PlanoPagamentoDeleteAction
     public function exec(string $uuid): void
     {
         $planoPagamento = $this->repository->find($uuid);
-        // $condicoesPagamento = CondicaoPagamento::where('plano_pagamento_uuid', $uuid)->get();
-        // $condicoesPagamento->delete();
         $planoPagamento->delete();
     }
 }
