a95868c1c50139e504fd450099c26f5c41fb4834
Luan Ramos <luan.developer@gmail.com>
2025-04-02 16:10:08 -0300

    corrige relatorio cliente

--- app/Http/Controllers/App/Mapa/Prelance/MapaResumoClienteShowController.php
+++ app/Http/Controllers/App/Mapa/Prelance/MapaResumoClienteShowController.php
@@ -33,14 +33,18 @@ class MapaResumoClienteShowController extends Controller
         $cliente = Cliente::where('uuid', $request->get('clienteUuid'))->first();
 
         $leilao = Leilao::with([
-            'config_prelance', 'clientes' => function($query) use ($request) {
+            'config_prelance', 
+            'clientes' => function($query) use ($request) {
                 $query->when(!is_null($request->get('clienteUuid')), function($query) use ($request) {
                     $query->where('uuid', $request->get('clienteUuid'));
                 });
             },  
-            'clientes.prelances.lote' => function($query) use ($request, $leilaoUuid) {
-                $query->where('leilao_uuid', $leilaoUuid);
-            }, 
+            'clientes.prelances' => function($query) use ($leilaoUuid) {
+                $query->whereHas('lote', function($q) use ($leilaoUuid) {
+                    $q->where('leilao_uuid', $leilaoUuid);
+                });
+            },
+            'clientes.prelances.lote',
             'clientes.prelances.prelance_config'
         ])->where('uuid', $leilaoUuid)->first();
         
