a4bc14f802e5d7e30eb45fb3a4794d97121fcd34
Luan Ramos <luan.developer@gmail.com>
2025-06-12 23:09:42 -0300

    titulo mensagens whatsapp

--- resources/views/livewire/components/app/pre-lance-visao-geral.blade.php
+++ resources/views/livewire/components/app/pre-lance-visao-geral.blade.php
@@ -365,14 +365,14 @@
                                        </td>
                                         <td class="px-3 py-2" style="text-align: right">
                                             @if($lance->uuid === $lance->lote->prelance_vencedor()->uuid)
-                                                <button onclick="copiarVencendo({{json_encode($lance)}})" type="button" class="ml-2 px-2 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 rounded-lg text-sm">
+                                                <button onclick="copiarVencendo('{{$leilao->descricao}}', {{json_encode($lance)}})" type="button" class="ml-2 px-2 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 rounded-lg text-sm">
                                                     <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                         <path fill="currentColor" fill-rule="evenodd" d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z" clip-rule="evenodd"/>
                                                         <path fill="currentColor" d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z"/>
                                                     </svg>
                                                 </button>
                                             @else
-                                                <button onclick="copiarSuperado({{json_encode($lance)}}, {{json_encode($lance->lote->prelance_vencedor())}})" type="button" class="ml-2 px-2 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 rounded-lg text-sm">
+                                                <button onclick="copiarSuperado('{{$leilao->descricao}}', {{json_encode($lance)}}, {{json_encode($lance->lote->prelance_vencedor())}})" type="button" class="ml-2 px-2 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 rounded-lg text-sm">
                                                     <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                                         <path fill="currentColor" fill-rule="evenodd" d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z" clip-rule="evenodd"/>
                                                         <path fill="currentColor" d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z"/>
@@ -1293,14 +1293,20 @@
 
    }
 
-     function copiarVencendo(lanceVencedor) {
+     function copiarVencendo(descricaoLeilao, lanceVencedor) {
          const valorFormatado = formatarMoedaBrasileira(lanceVencedor.valor);
          const dataLance = new Date(lanceVencedor.created_at).toLocaleDateString('pt-BR');
          const horaLance = new Date(lanceVencedor.created_at).toLocaleTimeString('pt-BR', {hour: '2-digit', minute:'2-digit'});
          const comissaoFormatada = lanceVencedor.prelance_config.percentual_comissao_comprador + '%';
          const valorComissao = formatarMoedaBrasileira(lanceVencedor.valor_comissao_compra);
 
-         let mensagem = `📢 *CONFIRMAÇÃO DE LANCE* 📢\n\n`;
+         let mensagem = "🅰️ *ATUAL LEILÕES E EVENTOS*\n";
+         mensagem += "*" + descricaoLeilao + "*\n\n";
+         mensagem += "======================\n";
+         mensagem += "📢 *LANCE CONFIRMADO* 📢\n";
+         mensagem += "======================\n";
+
+         mensagem += `\n`;
          mensagem += `✨ *Lote ${lanceVencedor.lote.numero}* - ${lanceVencedor.lote.descricao || 'Sem descrição'}\n\n`;
          mensagem += `💎 *Valor do Lance:* ${valorFormatado}\n`;
          mensagem += `📅 *Data/Hora:* ${dataLance} às ${horaLance}\n\n`;
@@ -1315,7 +1321,7 @@
          return mensagem;
      }
 
-     function copiarSuperado(lanceSuperado, lanceVencedor) {
+     function copiarSuperado(descricaoLeilao, lanceSuperado, lanceVencedor) {
          const valorSuperado = formatarMoedaBrasileira(lanceSuperado.valor);
          const valorVencedor = formatarMoedaBrasileira(lanceVencedor.valor);
          const diferenca = formatarMoedaBrasileira(lanceVencedor.valor - lanceSuperado.valor);
@@ -1323,7 +1329,13 @@
          const descricaoLote = lanceSuperado.lote.descricao || '';
          const comissaoFormatada = lanceVencedor.prelance_config.percentual_comissao_comprador + '%';
 
-         let mensagem = `📢 *LANCE SUPERADO* 📢\n\n`;
+         let mensagem = "🅰️ *ATUAL LEILÕES E EVENTOS*\n";
+         mensagem += "*" + descricaoLeilao + "*\n\n";
+         mensagem += "======================\n";
+         mensagem += "📢 *LANCE SUPERADO* 📢\n";
+         mensagem += "======================\n";
+
+         mensagem += `\n`;
          mensagem += `⚠️ Seu lance no *Lote ${numeroLote} ${descricaoLote}* foi superado por ${diferenca} reais\n\n`;
          mensagem += `🔹 *Seu lance:* ${valorSuperado}\n`;
          mensagem += `🔹 *Novo lance vencedor:* ${valorVencedor}\n`;
